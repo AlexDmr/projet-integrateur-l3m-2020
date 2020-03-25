3f01fe5cd5e4f20862c5931bd73e4bea0e5689e9
AlexDmr <demeurea@SurfacePro7Ng.localdomain>
2020-03-25 16:06:09 +0100

    Initial commit

---
+++ clientMenuCinema/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {TmdbService} from './tmdb.service';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [TmdbService, HttpClient],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
